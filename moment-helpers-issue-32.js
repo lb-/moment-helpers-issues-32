c8249f7243b6ac78699143ee3b9e090f42fdd1d5
LB (Ben) Johnston <github@lb.ee>
2015-12-14 16:36:49 +1000

    initial commit

---
+++ moment-helpers-issue-32.js
@@ -0,0 +1,26 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    },
+    someDate: function () {
+      return new Date('2015-02-13');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
